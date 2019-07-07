dfacfb43b2ac530250017de776e5a1b782199557
Sokolovskiy Mikhail <sokolmish@yandex.ru>
2019-07-07 19:41:27 +0300

    Fix bug with several spheres

--- main.cpp
+++ main.cpp
@@ -11,33 +11,31 @@
 #define BACKGROUND LIGHT_BLUE //Later background will be changed to skybox
 
 Color traceRay(Vec3f origin, Vec3f dir, std::vector<Sphere> objects, std::vector<Light> lights) {
-    bool intersected = false;
-    Color tCol = BACKGROUND;
+    Sphere *_target = NULL; 
     float tLen = __FLT_MAX__;
-    float lightIntense = 0.f;
     for (auto&& obj : objects) {
         float t = obj.intersect(origin, dir);
-        if (t >= 0) { // If ray intersect an object
-            if (t < tLen) {
-                intersected = true;
-
-                Vec3f touch = origin + t * dir;
-                tLen = t;
-                tCol = obj.color(touch);
-
-                for (auto&& light : lights) {
-                    Vec3f lDir = (light.loc - touch).normalize();
-                    float tPower = lDir * obj.normal(touch);
-                    if (tPower > 0)
-                        lightIntense += tPower * light.power;
-                }
-            }
+        if (t >= 0 && t < tLen) { // If ray intersect an object
+            _target = &obj;
+            tLen = t;
         }
     }
-    if (intersected)
-        return tCol * lightIntense;
-    else
+    if (_target == NULL)
         return BACKGROUND;
+
+    Sphere &target = *_target;
+    Vec3f touch = origin + tLen * dir;
+    Color diffCol = target.color(touch);
+    
+    float lightIntense = 0.f;
+    for (auto&& light : lights) {
+        Vec3f lDir = (light.loc - touch).normalize();
+        float tPower = lDir * target.normal(touch);
+        if (tPower > 0)
+            lightIntense += tPower * light.power;
+    }
+    
+    return diffCol * lightIntense;
 }
 
 int main(int argc, char **argv) {
@@ -45,10 +43,9 @@ int main(int argc, char **argv) {
     std::vector<Light> lights;
 
     objects.push_back(Sphere(Vec3f(0, 0, -14), 3, RED));
-    objects.push_back(Sphere(Vec3f(2, 4, -14.5), 1.5, BLUE));
+    objects.push_back(Sphere(Vec3f(3, 4.5, -12.5), 2.5, BLUE));
 
-    lights.push_back(Light(Vec3f(10, 25, -12), 1));
-    lights.push_back(Light(Vec3f(-9, 5, -10), 1.2));
+    lights.push_back(Light(Vec3f(10, 25, 1), 1.2));
 
     int width = 1024; //Resolution
     int height = 768;
